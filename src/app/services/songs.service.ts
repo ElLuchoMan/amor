bdf2d96c0c374bff8d0af02f6308ce0f0f5e8589
ElLuchoMan <baluist@correo.udistrital.edu.co>
2024-04-15 19:30:03 -0500

    Added: service to get songs

---
+++ src/app/services/songs.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SongsService {
+  url = 'https://api.jsonstorage.net/v1/json/6426abfb-bbae-43e1-94d8-70c6cbb1554e/3c955013-acd4-4290-99a9-621ecf24b868';
+  
+  private http = inject ( HttpClient);
+
+  listSongs() {
+    return this.http.get(this.url);
+  }
+
+}
