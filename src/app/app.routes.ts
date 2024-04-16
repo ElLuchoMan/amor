41b43951d3ff4ecaee42d7721f67d5a907d57412
ElLuchoMan <baluist@correo.udistrital.edu.co>
2024-04-15 21:28:06 -0500

    Fixed: some things

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HelloComponent } from './components/hello/hello.component';
 import { SongsComponent } from './components/songs/songs.component';
+import { SayYesComponent } from './components/say-yes/say-yes.component';
 
 export const routes: Routes = [
     { path: 'hello', component: HelloComponent },
     { path:'songs', component: SongsComponent},
+    {path:'say-yes', component: SayYesComponent},
     { path: '**', redirectTo: '/hello' },
 ];
