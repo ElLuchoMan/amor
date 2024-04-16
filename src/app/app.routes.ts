1b0ce16f38ede356c761c6d5c413bb109902dff1
ElLuchoMan <baluist@correo.udistrital.edu.co>
2024-04-15 21:21:04 -0500

    Added: some things

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HelloComponent } from './components/hello/hello.component';
+import { SongsComponent } from './components/songs/songs.component';
 
 export const routes: Routes = [
     { path: 'hello', component: HelloComponent },
-    { path: '**', redirectTo: '/hello' }
+    { path:'songs', component: SongsComponent},
+    { path: '**', redirectTo: '/hello' },
 ];
