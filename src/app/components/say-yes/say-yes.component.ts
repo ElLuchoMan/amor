b7de17b031c5a75fb8e7f0446e8f48dbfbf6ee67
ElLuchoMan <baluist@correo.udistrital.edu.co>
2024-04-16 09:11:26 -0500

    Added: image and buttons

--- src/app/components/say-yes/say-yes.component.ts
+++ src/app/components/say-yes/say-yes.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SongsService } from '../../services/songs.service';
 
 @Component({
   selector: 'app-say-yes',
@@ -8,10 +9,27 @@ import { Router } from '@angular/router';
   templateUrl: './say-yes.component.html',
   styleUrl: './say-yes.component.scss'
 })
-export class SayYesComponent {
+export class SayYesComponent implements OnInit {
+  ngOnInit() {
+    this.getIamge();
+  }
+
   private router = inject(Router);
+  private service = inject(SongsService);
+
+  image = '../../../assets/imagen.jpeg';
 
   goToPage(pageName: string){
     this.router.navigate([`${pageName}`]);
+  }
+  getIamge(){
+    this.service.listSongs().subscribe((data:any)=>{
+      this.image = data.image;
+      console.log(data.image);
+    })
+  }
+
+  salirDePagina(){
+    window.location.href = 'https://youtu.be/7oKZeo779Xg?si=4SM-GqgcJYqmGLNw&t=103';
   }
 }
