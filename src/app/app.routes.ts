18728f1bab0a9c7118ede9824998b15554cc9d0c
ElLuchoMan <baluist@correo.udistrital.edu.co>
2024-06-29 20:17:44 -0500

    Added: ahorcado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { HelloComponent } from './components/hello/hello.component';
 import { SongsComponent } from './components/songs/songs.component';
 import { SayYesComponent } from './components/say-yes/say-yes.component';
+import { TristezaComponent } from './components/tristeza/tristeza.component';
 
 export const routes: Routes = [
+    { path: '', component: HelloComponent },
     { path: 'hello', component: HelloComponent },
-    { path:'songs', component: SongsComponent},
-    {path:'say-yes', component: SayYesComponent},
-    { path: '**', component: HelloComponent },
+    { path: 'songs', component: SongsComponent },
+    { path: 'say-yes', component: SayYesComponent },
+    { path: 'no-estes-triste', component: TristezaComponent },
+    { path: '**', component: TristezaComponent },
 ];
