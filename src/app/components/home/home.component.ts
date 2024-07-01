1b00161eafdf3e0a71f96cda028f6741f994cd9f
ElLuchoMan <baluist@correo.udistrital.edu.co>
2024-07-01 13:43:24 -0500

    Added: images TODO: refactor to styles Se agregan las imágenes para las cardas,  queda pendiente modificar los estilos para que se vean mejor

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 
 interface Card {
   title: string;
-  // imageUrl: string;
+  imageUrl: string;
   description: string;
   route: string;
 }
@@ -22,25 +22,25 @@ export class HomeComponent {
   cards: Card[] = [
     {
       title: 'Hello Page',
-      // imageUrl: 'assets/hello.jpg',
+      imageUrl: 'assets/hello.jpg',
       description: 'Go to the Hello Page',
       route: '/hello'
     },
     {
       title: 'Songs Page',
-      // imageUrl: 'assets/songs.jpg',
+      imageUrl: 'assets/songs.jpg',
       description: 'Go to the Songs Page',
       route: '/songs'
     },
     {
       title: 'Say Yes Page',
-      // imageUrl: 'assets/say-yes.jpg',
+      imageUrl: 'assets/say-yes.jpg',
       description: 'Go to the Say Yes Page',
       route: '/say-yes'
     },
     {
       title: 'No Estes Triste',
-      // imageUrl: 'assets/no-estes-triste.jpg',
+      imageUrl: 'assets/no-estes-triste.jpg',
       description: 'Go to the No Estes Triste Page',
       route: '/no-estes-triste'
     }
