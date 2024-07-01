6a87ebcdd5eaf55707c649ca1a0dc19422f8a641
ElLuchoMan <baluist@correo.udistrital.edu.co>
2024-07-01 13:36:02 -0500

    Added: Home component TODO: add images for this page Se agrega todo el nuevo componente de Home, queda pendiente crear las imágnes para agregarlas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { HelloComponent } from './components/hello/hello.component';
 import { SongsComponent } from './components/songs/songs.component';
 import { SayYesComponent } from './components/say-yes/say-yes.component';
 import { TristezaComponent } from './components/tristeza/tristeza.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: HelloComponent },
+    { path: '', component: HomeComponent },
     { path: 'hello', component: HelloComponent },
     { path: 'songs', component: SongsComponent },
     { path: 'say-yes', component: SayYesComponent },
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface Card {
+  title: string;
+  // imageUrl: string;
+  description: string;
+  route: string;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  cards: Card[] = [
+    {
+      title: 'Hello Page',
+      // imageUrl: 'assets/hello.jpg',
+      description: 'Go to the Hello Page',
+      route: '/hello'
+    },
+    {
+      title: 'Songs Page',
+      // imageUrl: 'assets/songs.jpg',
+      description: 'Go to the Songs Page',
+      route: '/songs'
+    },
+    {
+      title: 'Say Yes Page',
+      // imageUrl: 'assets/say-yes.jpg',
+      description: 'Go to the Say Yes Page',
+      route: '/say-yes'
+    },
+    {
+      title: 'No Estes Triste',
+      // imageUrl: 'assets/no-estes-triste.jpg',
+      description: 'Go to the No Estes Triste Page',
+      route: '/no-estes-triste'
+    }
+  ];
+
+  navigateTo(route: string): void {
+    this.router.navigate([route]);
+  }
 }
