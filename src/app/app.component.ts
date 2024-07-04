3adff2400702fc082aed1e5f09c31687d9d08994
ElLuchoMan <baluist@correo.udistrital.edu.co>
2024-07-04 17:26:35 -0500

    Revert "Added: notifications service"
    This reverts commit 04f26babd9ec046a63cf5e2df325509ee4b9bf64.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { SongsService } from './services/songs.service';
 
 @Component({
   selector: 'app-root',
@@ -11,54 +10,6 @@ import { SongsService } from './services/songs.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'amor';
-  readonly publicVapidKey = 'BI4YQ9kIan2m3EqyM0dFx3PYBUF3Ih3SMTdsqWWW--pClBQK9wywpAKI2smlpJyk4g9pRN22bUM54lrgzTZ8DQ4';
-
-  constructor(private songsService: SongsService) {}
-
-  ngOnInit(): void {
-    this.requestPermissionAndSubscribeToPush();
-  }
-
-  private async requestPermissionAndSubscribeToPush(): Promise<void> {
-    if ('serviceWorker' in navigator && 'PushManager' in window) {
-      const registration = await navigator.serviceWorker.ready;
-
-      const permission = await Notification.requestPermission();
-
-      if (permission === 'granted') {
-        try {
-          const subscription = await registration.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: this.urlBase64ToUint8Array(this.publicVapidKey)
-          });
-
-          this.songsService.sendSubscriptionToServer(subscription).subscribe({
-            next: () => console.log('Suscripción enviada al servidor.'),
-            error: (err) => console.error('Error al enviar la suscripción al servidor:', err)
-          });
-        } catch (err) {
-          console.error('Fallo al suscribirse a las notificaciones push:', err);
-        }
-        Notification.requestPermission().then(function(permission) {
-          console.log('Notification permission:', permission);
-        });
-        
-      }
-    }
-  }
-
-  private urlBase64ToUint8Array(base64String: string): Uint8Array {
-    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
-    const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
-
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
-
-    for (let i = 0; i < rawData.length; ++i) {
-      outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
-  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,8 @@
-import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, RouterModule } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr, ToastrModule } from 'ngx-toastr';
-import { provideServiceWorker } from '@angular/service-worker';
 
 import { routes } from './app.routes';
 
@@ -18,10 +17,6 @@ export const appConfig: ApplicationConfig = {
       positionClass: 'toast-top-center',
       preventDuplicates: true,
     })),
-    importProvidersFrom(RouterModule.forRoot(routes)),
-    provideServiceWorker('../ngsw-config.json', {
-      enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+    importProvidersFrom(RouterModule.forRoot(routes))
   ]
 };
