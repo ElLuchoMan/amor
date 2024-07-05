f1ea077cdb62eba261c9e9e266e3e87092672541
ElLuchoMan <baluist@correo.udistrital.edu.co>
2024-07-04 19:55:45 -0500

    Added: cache clean Se agrega funcionalidad para borrar caché cada 24 horas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -10,6 +10,39 @@ import { FooterComponent } from './components/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'amor';
+
+  ngOnInit() {
+    this.scheduleCacheUpdate();
+  }
+
+  scheduleCacheUpdate() {
+    const now = new Date();
+    const hoursUntilMidnight = 24 - now.getHours();
+    const minutesUntilMidnight = 60 - now.getMinutes();
+    const secondsUntilMidnight = 60 - now.getSeconds();
+    const millisecondsUntilMidnight = ((hoursUntilMidnight * 60 + minutesUntilMidnight) * 60 + secondsUntilMidnight) * 1000;
+
+    setTimeout(() => {
+      this.updateCache();
+      setInterval(this.updateCache, 24 * 60 * 60 * 1000);
+    }, millisecondsUntilMidnight);
+  }
+
+  updateCache() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations().then(registrations => {
+        registrations.forEach(registration => {
+          registration.update().then(() => {
+            if (registration.waiting) {
+              registration.waiting.postMessage({ action: 'skipWaiting' });
+            }
+          });
+        });
+      });
+    }
+  }
+
+
 }
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,22 +1,45 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('v1').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles.css',
-          '/script.js',
-          '/assets/hello.jpg'
-        ]);
+const CACHE_NAME = 'my-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/assets/hello.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
       })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
       })
-    );
-  });
-  
+  );
+});
