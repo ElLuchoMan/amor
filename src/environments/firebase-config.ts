5391c6d58a12b5462b037c3a272ae49add8b3c42
ElLuchoMan <baluist@correo.udistrital.edu.co>
2024-07-05 16:57:14 -0500

    Added: notifications service

--- src/environments/firebase-config.ts
+++ src/environments/firebase-config.ts
@@ -5,14 +5,15 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCYAUsOFhucyXzCE2orEnT-_FATBTvw9cA",
-  authDomain: "natyalejo-d82e2.firebaseapp.com",
-  projectId: "natyalejo-d82e2",
-  storageBucket: "natyalejo-d82e2.appspot.com",
-  messagingSenderId: "89155861645",
-  appId: "1:89155861645:web:91f5ed51029e035f5a76c4",
-  measurementId: "G-HSV6LQVDH8"
+export const firebaseConfig = {
+    apiKey: "AIzaSyCYAUsOFhucyXzCE2orEnT-_FATBTvw9cA",
+    authDomain: "natyalejo-d82e2.firebaseapp.com",
+    projectId: "natyalejo-d82e2",
+    storageBucket: "natyalejo-d82e2.appspot.com",
+    messagingSenderId: "89155861645",
+    appId: "1:89155861645:web:91f5ed51029e035f5a76c4",
+    measurementId: "G-HSV6LQVDH8",
+    vapidKey: "BI-L9JSRv9h8lb39CQYbnW5IBEx7MMGhn6x_Wbe1GF_XwXQ56fcGpRao0j8Ex-PkzwYMwr1JYJIP2qHPyZHeNjs"
 };
 
 // Initialize Firebase
---
+++ src/firebase-admin-sdk.js
@@ -0,0 +1,48 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./path/to/firebase-admin-sdk.json');
+const cron = require('node-cron');
+const { SongsService } = require('../src/app/services/songs.service');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const songsService = new SongsService();
+const tokens = songsService.tokenFCM;
+
+const sendNotification = (title, body) => {
+  const messages = tokens.map(token => {
+    return {
+      token: token,
+      notification: {
+        title: title,
+        body: body
+      }
+    };
+  });
+
+  admin.messaging().sendEach(messages)
+    .then((response) => {
+      console.log(response.successCount + ' mensajes enviados exitosamente');
+    })
+    .catch((error) => {
+      console.log('Error enviando el mensaje:', error);
+    });
+};
+
+
+cron.schedule('0 12 * * *', () => {
+  sendNotification('Hola', 'Solo quería desearte un feliz almuerzo');
+});
+
+// Tarea para enviar notificación en la noche
+cron.schedule('0 20 * * *', () => {
+  sendNotification('Buenas noches', 'Espero que descanses mucho y duermas bien, sueña lindo.');
+});
+
+// Ejemplo de tarea personalizada durante el día
+cron.schedule('0 15 * * *', () => {
+  sendNotification('Recordatorio', 'No olvides que a pesar de estar ocupado, te amo demasiasdo');
+});
+
+console.log('Tareas programadas configuradas.');
