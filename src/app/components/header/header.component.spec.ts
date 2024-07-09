2825f35965320c42ff32c3f2d6d69312d7b19ad7
ElLuchoMan <baluist@correo.udistrital.edu.co>
2024-07-09 12:33:13 -0500

    Added: app component tests

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -2,15 +2,23 @@
 
 import { TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
-import { ToastrModule, TOAST_CONFIG } from 'ngx-toastr';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SongsService } from '../../services/songs.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('HeaderComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ToastrModule.forRoot()],
-      declarations: [HeaderComponent],
+      imports: [
+        ToastrModule.forRoot(),
+        RouterTestingModule,
+        HttpClientTestingModule,
+        HeaderComponent // Importar el componente como independiente
+      ],
       providers: [
-        { provide: TOAST_CONFIG, useValue: {} }
+        SongsService,
+        ToastrService
       ]
     }).compileComponents();
   });
@@ -19,5 +27,24 @@ describe('HeaderComponent', () => {
     const fixture = TestBed.createComponent(HeaderComponent);
     const component = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  it('should get UUID from songService on init', () => {
+    const fixture = TestBed.createComponent(HeaderComponent);
+    const component = fixture.componentInstance;
+    const songService = TestBed.inject(SongsService);
+    const uuid = 'mock-uuid';
+    jest.spyOn(songService, 'getUUID').mockReturnValue(uuid);
+    fixture.detectChanges();
+    expect(component.user_id).toBe(uuid);
+  });
+
+  it('should copy UUID to clipboard', async () => {
+    const fixture = TestBed.createComponent(HeaderComponent);
+    const component = fixture.componentInstance;
+    component.user_id = 'mock-uuid';
+    const writeTextSpy = jest.spyOn(navigator.clipboard, 'writeText').mockResolvedValue();
+    await component.copyTokenToClipboard();
+    expect(writeTextSpy).toHaveBeenCalledWith('mock-uuid');
   });
 });
