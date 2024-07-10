56b54e809bf3466fcc43a96b448bcd0d8e88c65a
ElLuchoMan <baluist@correo.udistrital.edu.co>
2024-07-09 19:51:10 -0500

    Added: get resources fix

--- functions/get-resources.js
+++ functions/get-resources.js
@@ -1,18 +1,42 @@
 const faunadb = require('faunadb');
 const q = faunadb.query;
 
-exports.handler = async (event, context) => {
-  const client = new faunadb.Client({ secret: process.env.FAUNA_SECRET });
+exports.handler = async (event) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ message: 'Options Request' }),
+    };
+  }
+
+  if (event.httpMethod !== 'GET') {
+    return {
+      statusCode: 405,
+      headers,
+      body: JSON.stringify({ message: 'Method Not Allowed' }),
+    };
+  }
+
+  const client = new faunadb.Client({ secret: process.env.FAUNADB_SECRET });
 
   try {
     const result = await client.query(q.Get(q.Ref(q.Collection('resources'), 'resources')));
     return {
       statusCode: 200,
+      headers,
       body: JSON.stringify(result.data),
     };
   } catch (error) {
     return {
       statusCode: 500,
+      headers,
       body: JSON.stringify({ message: 'Error fetching resources', error: error.message }),
     };
   }
