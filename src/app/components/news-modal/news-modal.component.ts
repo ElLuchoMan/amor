1084092ee4cd75acb42cffd6468189ab95a03fbf
ElLuchoMan <baluist@correo.udistrital.edu.co>
2024-07-15 18:24:55 -0500

    Added: News modal based on version app

--- src/app/components/news-modal/news-modal.component.ts
+++ src/app/components/news-modal/news-modal.component.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import * as bootstrap from 'bootstrap';
 import { SongsService } from '../../services/songs.service';
 import { CommonModule } from '@angular/common';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-news-modal',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './news-modal.component.html',
-  styleUrl: './news-modal.component.scss'
+  styleUrls: ['./news-modal.component.scss']
 })
 export class NewsModalComponent implements OnInit {
   nuevosCambios: string[] = [];
+  appVersion: string = environment.appVersion;
 
   constructor(private songsService: SongsService) { }
 
@@ -21,10 +23,22 @@ export class NewsModalComponent implements OnInit {
   }
 
   checkForChanges(): void {
-    const cambiosAceptados = localStorage.getItem('cambiosAceptados');
-    if (!cambiosAceptados || cambiosAceptados !== 'true') {
+    const cambiosInfo = localStorage.getItem('cambiosInfo');
+    const cambiosInfoParsed = cambiosInfo ? JSON.parse(cambiosInfo) : null;
+    
+    if (!cambiosInfoParsed || !this.isSameVersion(cambiosInfoParsed.appVersion) || !cambiosInfoParsed.viewed) {
       this.mostrarCambiosModal();
     }
+  }
+
+  isSameVersion(storedVersion: string): boolean {
+    if (!storedVersion) {
+      return false;
+    }
+    const [currentMajor, currentMinor, currentPatch] = this.appVersion.split('.').map(Number);
+    const [storedMajor, storedMinor, storedPatch] = storedVersion.split('.').map(Number);
+
+    return currentMajor === storedMajor && currentMinor === storedMinor && currentPatch === storedPatch;
   }
 
   mostrarCambiosModal(): void {
@@ -36,7 +50,11 @@ export class NewsModalComponent implements OnInit {
   }
 
   aceptarCambios(): void {
-    localStorage.setItem('cambiosAceptados', 'true');
+    const cambiosInfo = {
+      appVersion: this.appVersion,
+      viewed: true
+    };
+    localStorage.setItem('cambiosInfo', JSON.stringify(cambiosInfo));
     const modalElement = document.getElementById('cambiosModal');
     if (modalElement) {
       const cambiosModal = bootstrap.Modal.getInstance(modalElement) as bootstrap.Modal;
@@ -46,7 +64,8 @@ export class NewsModalComponent implements OnInit {
 
   getChanges(): void {
     this.songsService.getChanges().subscribe((data: any) => {
-      this.nuevosCambios = data?.changes;
+      console.log('Data:', data);
+      this.nuevosCambios = data.map((item: any) => item.change);
     }, (error: any) => {
       console.error('Error al obtener los cambios', error);
     });
