5baef78e0fec79ca7d191e7ecec4a0d1146e7a4d
ElLuchoMan <baluist@correo.udistrital.edu.co>
2024-07-21 21:43:45 -0500

    Fixed: added controls for mobile

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    appVersion: '1.6.0',
+    appVersion: '1.6.1',
     production: false,
     jsonUrl: 'https://api.jsonstorage.net/v1/json/6426abfb-bbae-43e1-94d8-70c6cbb1554e/3c955013-acd4-4290-99a9-621ecf24b868',
-    //apiUrl: 'http://localhost:3000/api'
-    apiUrl: 'https://amornatyalejo.netlify.app/.netlify/functions'
+    apiUrl: 'http://localhost:3000/api'
+    //apiUrl: 'https://amornatyalejo.netlify.app/.netlify/functions'
 };
