7d0c542f143800ac616ee107441b0ed2669eae5f
ElLuchoMan <baluist@correo.udistrital.edu.co>
2024-12-15 19:10:28 -0500

    Reverted: sort in carts Se revierte la organización de las cartas para mobile

--- src/app/pages/letters-page/letters-page.component.ts
+++ src/app/pages/letters-page/letters-page.component.ts
@@ -13,20 +13,16 @@ import { LettersService } from '../../services/letters.service';
 })
 export class LettersPageComponent implements OnInit {
   letters: { date: string, image: string, text: string }[] = [];
-  isMobile = false;
 
   constructor(private router: Router, private lettersService: LettersService) { }
 
   ngOnInit(): void {
-    this.isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     this.loadLetters();
   }
 
   loadLetters(): void {
     this.lettersService.getLetters().subscribe(data => {
-      this.letters = this.isMobile
-        ? data
-        : data.sort((a, b) => this.transformDate(b.date).getTime() - this.transformDate(a.date).getTime());
+      this.letters = data.sort((a, b) => this.transformDate(b.date).getTime() - this.transformDate(a.date).getTime());
     }, error => {
       console.error('Error fetching letters:', error);
     });
