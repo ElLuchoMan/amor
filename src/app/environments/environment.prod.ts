c957bf10e64042f0cf27835b67d4d3630f3ad6ef
ElLuchoMan <baluist@correo.udistrital.edu.co>
2024-12-17 17:05:19 -0500

    Added: new version

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    appVersion: '1.6.2',
+    appVersion: '2.0.0',
     production: true,
     jsonUrl: 'https://api.jsonstorage.net/v1/json/6426abfb-bbae-43e1-94d8-70c6cbb1554e/3c955013-acd4-4290-99a9-621ecf24b868',
     apiUrl: 'https://amornatyalejo.netlify.app/.netlify/functions'
